26b921da9ef1ff51d93464aa50341ce2df96bf84
Drew Kang <kangdrew95@gmail.com>
2021-07-24 03:10:27 +0000

    Took a while but fixed recursion issue I had

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+
+  // if (array.length === 0) {
+  //   return 0;
+  // }
+
+  // if (Array.isArray(array[0])) {
+  //   return sumItems(array[0]);
+  // } else {
+  //   return array[0] + sumItems(array.slice(1));
+  // }
+
+  let retVal = 0;
+
+  for (const val of array) {
+    if (Array.isArray(val)) {
+      retVal += sumItems(val);
+    } else {
+      retVal += val;
+    }
+  }
+  return retVal;
 }
 
 module.exports = sumItems;
